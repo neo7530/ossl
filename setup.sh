9427b6266ad1eb33683cf67dfa71778dfcb38981
root <root@pi-star>
2024-08-26 17:44:58 +0100

    ...

--- setup.sh
+++ setup.sh
@@ -11,13 +11,6 @@ echo 'export PATH'>> /etc/profile.d/openssl.sh
 chmod +x /etc/profile.d/openssl.sh
 source /etc/profile.d/openssl.sh
 openssl version -a
-rm -r /var/www/dashboard
 rm -r /usr/local/sbin
-rm -r /usr/local/bin
-git clone https://github.com/neo7530/pistar_dash /var/www/dashboard
-# cp -r /tmp/dashboard/.git /var/www/dashboard/
 git clone https://github.com/neo7530/pistar_sbin /usr/local/sbin
-# cp -r /tmp/sbin/.git /usr/local/sbin/
-git clone https://github.com/neo7530/pistar_bin /usr/local/bin
-# cp -r /tmp/bin/.git /usr/local/bin/
 pistar-update
